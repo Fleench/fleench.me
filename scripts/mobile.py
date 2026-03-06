88297287cc985a18d7fe1e20cc8216f94c0d341b
Flench04 <47568518+Fleench@users.noreply.github.com>
2026-03-06 23:36:24 +0000

    start prep for mobile

---
+++ scripts/mobile.py
@@ -0,0 +1,6 @@
+def main(src_dir, out_dir, config):
+    print("---")
+    print("Welcome to mobile buildier")
+    print("---")
+    print("This plugin is not implimented. But it will be making a mobile freindly site hardlinking any files except .element, .html, .html.temp, or .md files to dist/mobile. It will then be taking all notes and blogs and bulding them to be mobile friendly by removing everything but the content and author information.")
+    print("---")
