5b15d588a555043ffffef3d1dc1f1ae808cb5e5f
Fleench <social.glenn@flench.me>
2026-03-14 13:28:49 -0600

    Started changes to add web ratings

---
+++ src/.elements/rate.py
@@ -0,0 +1,2 @@
+def main(**args):
+    return """<img style="height: 100%" alt="A cute green monster showing the text WebG in red tounge. Represents the page rating." src="/media/webRatings/WebG.png">"""
