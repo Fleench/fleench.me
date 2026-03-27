82bb757d96d946b7ed587c31f93bb8fab29b78e9
Fleench <social.glenn@flench.me>
2026-03-27 10:05:43 -0600

    Sitemap code almost done

--- scripts/map.py
+++ scripts/map.py
@@ -1,6 +1,63 @@
+class Node:
+    def __init__(self, name: str, value: str, parent = None):
+        self.name = name
+        self.children = {}
+        self.parent = parent
+        self.value = value
+    def add_child(self, node):
+        self.children[node.name] = node
+    def add_parent(self, node):
+        self.parent = node
+    def add_value(self, value):
+        self.value = value
+    def get_children(self):
+        return self.children
+    def get_parent(self):
+        return self.parent
+    def get_value(self):
+        return self.value
+    def get_name(self):
+        return self.name
+    def get_child(self, name):
+        return self.children.get(name)
+    def __str__(self):
+        return str(f"Name: {self.name}, Value: {self.value}, Children: {self.children}")
+def print_nodes(t_node: Node, deep:int):
+    build = []
+    for node in t_node.children.values():
+        if not node.name:
+            print("Err")
+        item = f"{"-"*deep}{node.name},{node.value}"
+        build.append(item)
+        if node.get_children() is not {}:
+            #print("\n")
+            x = print_nodes(node, deep+1)
+            for item in x:
+                build.append(item)
+    return build
 def main(src_dir, out_dir, config, files):
+    print("INIT")
+    top_node = Node("Top Node", 0)
     for page in files:
+        _ = page.parse_content()
         p = page.parsed.metadata
         #print(p)
         if not p.get("hidden",False):
-            print(page.canonical.split("https://flench.me")[1].split("/")[1:-1])
+            parts = page.rel_url.split("/")
+            cur_node = top_node
+            used_parts = []
+            for part in parts:
+                used_parts.append(part)
+                if part.strip() == "":
+                    continue
+                n_node = cur_node.children.get(part)
+                if n_node:
+                    cur_node = n_node
+                else:
+                    n_node = Node(part, "https://flench.me"+"/".join(used_parts), parent = cur_node)
+                    cur_node.add_child(n_node)
+                    cur_node = n_node
+    print("PRINTING NODES")
+    z = print_nodes(top_node, 0)
+    for item in z:
+        print(item)
--- src/todo.md
+++ src/todo.md
@@ -14,5 +14,6 @@
 - Update CMS bot to allow me to easily edit the todo list as I get ideas
 - Update CMS bot to allow different note types. (likes, notes, short-replies)
 - Update CMS bot to allow remote build starts
+- Allow the SSG to pass all pages to dynamic elements via the context object
 # Site Issues
 - Fix RSS Generation issues
